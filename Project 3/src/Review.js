ff8a8f1d95a68ecd4f9b2d2f5c2351b4c69fa0b4
Alex <1995aha@gmail.com>
2022-08-27 16:27:35 +0100

    project 3 second solution

--- Project 3/src/Review.js
+++ Project 3/src/Review.js
@@ -4,38 +4,81 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
 
-  const [userIndex, setUserIndex] = useState(1)
+  const [personIndex, setPersonIndex] = useState(0)
+  const { id, name, job, image, text } = people[personIndex]
 
-  const handleclick = (e) => {
-    if (e.target.value === "right") {
-      setUserIndex(userIndex + 1)
-      if (userIndex === 5) {
-        setUserIndex(1)
-      }
+  const checkNumber = (number) => {
+    if (number > people.length -1) {
+      return 0
+    } else if (number < 0 ) {
+      return people.length - 1
+    } else {
+      return number
     }
-    else if (e.target.value === "left") {
-      setUserIndex(userIndex - 1)
-      if (userIndex === 0) {
-        setUserIndex(4)
-      }
-    }
-    console.log(userIndex)
   }
+
+  const leftReview = () => {
+    setPersonIndex((personIndex) => {
+      let newPersonIndex = personIndex + 1
+      return checkNumber(newPersonIndex)
+    })
+  }
+
+  const rightReview = () => {
+    setPersonIndex((personIndex) => {
+      let newPersonIndex = personIndex - 1
+      return checkNumber(newPersonIndex)
+    })
+
+  }
+
   return (
     <>
-      {people.map((person, index) => {
-        const { id, name } = person
-        if (id == userIndex)
-        return (
-          <div key={index}>
-            {name}
-          </div>
-        )
-      })}
-      <button value="left" onClick={handleclick}>Left</button>
-      <button value="right" onClick={handleclick}>Right</button>
+      <div>
+        <p>{name}</p>
+        <p>{job}</p>
+        <img src={image} alt={name} />
+        <p>{text}</p>
+        <button onClick={leftReview}> <FaChevronLeft></FaChevronLeft> </button>
+        <button onClick={rightReview}> <FaChevronRight></FaChevronRight> </button>
+        <button>Surpise Person</button>
+      </div>
     </>
   )
+
+
+  // const [userIndex, setUserIndex] = useState(1)
+
+  // const handleclick = (e) => {
+  //   if (e.target.value === "right") {
+  //     setUserIndex(userIndex + 1)
+  //     if (userIndex === 5) {
+  //       setUserIndex(1)
+  //     }
+  //   }
+  //   else if (e.target.value === "left") {
+  //     setUserIndex(userIndex - 1)
+  //     if (userIndex === 0) {
+  //       setUserIndex(4)
+  //     }
+  //   }
+  //   console.log(userIndex)
+  // }
+  // return (
+  //   <>
+  //     {people.map((person, index) => {
+  //       const { id, name } = person
+  //       if (id == userIndex)
+  //       return (
+  //         <div key={index}>
+  //           {name}
+  //         </div>
+  //       )
+  //     })}
+  //     <button value="left" onClick={handleclick}>Left</button>
+  //     <button value="right" onClick={handleclick}>Right</button>
+  //   </>
+  // )
 }
 
 export default Review;
