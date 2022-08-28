2be44ffe923699c57d6f278b09241f6fb14dc6a1
Alex <1995aha@gmail.com>
2022-08-28 11:56:15 +0100

    finished filtering, doing styling

--- Project 5/src/App.js
+++ Project 5/src/App.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
+import menu from './data';
 
 function App() {
 
   const [categories, setCategories] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState("all")
+
   useEffect(() => {
     const buttons = () => {
       let onlyButtons = items.map(button => {
@@ -20,8 +23,27 @@ function App() {
   return (
     <>
       {categories.map(category => {
-        return <button>{category}</button>
+        return <button value={category} onClick={() => setSelectedCategory(category)}>{category}</button>
       })}
+      <section>
+        {menu.map(item => {
+          const { id, title, category, price, img, desc } = item
+          if (selectedCategory === "all") {
+            return (
+              <>
+                <p>{title}</p>
+              </>
+            )}
+
+          else if (selectedCategory === category) {
+            return (
+              <>
+                <p>{title}</p>
+              </>
+            )
+          }
+        })}
+      </section>
     </>
   )
 }
