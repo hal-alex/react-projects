539ad8def30ad479c91a5d8b019e6d94ab530eb8
Alex <1995aha@gmail.com>
2022-08-29 17:31:20 +0100

    finished take 1 and no styling done

--- Project 6/src/App.js
+++ Project 6/src/App.js
@@ -1,10 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
+import Jobs from './Jobs'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [apiResponse, setAPIResponse] = useState([])
+  const [personIndex, setPersonIndex] = useState(0)
+
+  useEffect(() => {
+    setIsLoading(true)
+    const pullData = async () => {
+      const response = await fetch(url)
+      const jsonResponse = await response.json()
+      setAPIResponse(jsonResponse)
+      setIsLoading(false)
+    }
+    pullData()
+  }, [])
+  
+
+  return (
+    <>
+      {isLoading ? "Loading..." : <Jobs apiResponse={apiResponse}></Jobs> }
+    </>
+  )
 }
 
 export default App
---
+++ Project 6/src/Jobs.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+
+const Jobs = (props) => {
+
+  const [displayJob, setDisplayJob] = useState("recAGJfiU4CeaV0HL")
+
+  const makeButtons = () => {
+    return ( <>
+      {props.apiResponse.map((job) => {
+        const { company, id } = job
+        return (
+          <>
+            <button key={job} onClick={() => setDisplayJob(id) }>{company}</button>
+          </>
+        )
+      })}
+    </>
+    )
+  }
+
+  return (
+    <>
+      {makeButtons()}
+      {props.apiResponse.map(item => {
+        const { id, title } = item
+        if (id === displayJob) {
+          return (
+            <>
+              <p>{title}</p>
+            </>
+          )
+        }
+      })}
+    </>
+  )
+}
+
+export default Jobs
