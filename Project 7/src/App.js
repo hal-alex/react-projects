9babf17909ed413e5ca6986140884d9f10a441fc
Alex <1995aha@gmail.com>
2022-08-31 00:11:32 +0100

    core functionality finished

--- Project 7/src/App.js
+++ Project 7/src/App.js
@@ -3,7 +3,39 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+
+  const [selectedIndex, setSelectedIndex] = useState(0)
+
+  const previousReview = () => {
+    let newVar = selectedIndex - 1
+    if (newVar < 0 ) {
+      setSelectedIndex(data.length - 1)
+    } else {
+      setSelectedIndex(selectedIndex - 1)
+    }
+  }
+  const nextReview = () => {
+    let newVar = selectedIndex + 1
+    if (newVar === data.length) {
+      setSelectedIndex(0)
+    } else {
+      setSelectedIndex(selectedIndex + 1)
+    }
+    
+  }
+
+  return (
+    <>
+    <button onClick={previousReview}>Prev</button>
+      <button onClick={nextReview}>Next</button>
+      {data[selectedIndex].name}
+      {/* {data.map(item => {
+        const { id, name } = item
+        return name
+
+      })} */}
+    </>
+  )
 }
 
 export default App;
