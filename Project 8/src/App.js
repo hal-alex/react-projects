cd203cbf8673eb7f9f6663cfce29c705dcd39df3
Alex <1995aha@gmail.com>
2022-09-01 23:48:46 +0100

    working on solution 2

--- Project 8/src/App.js
+++ Project 8/src/App.js
@@ -2,9 +2,47 @@ import React, { useState } from 'react';
 import data from './data';
 function App() {
   
+  const [paras, setParas] = useState(0)
+  const [text, setText] = useState([])
+
+  // const handleChange = (e) => {
+  //   setParas(e.target.value)
+  // }
   
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const name = e.target.name
+    const value = e.target.value
+    setParas(value)
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
+    <>
+    <h1>Random Text Generator</h1>
+      <form>
+        <label htmlFor="Paragraphs">Paras
+          <input 
+          type="number" 
+          name="paras" 
+          id="paras"
+          placeholder="0"
+          // onChange={handleChange}
+           />
+        </label>
+        <button onClick={handleSubmit}>generate</button>
+      </form>
+      <div>
+        {text.map((lorem, index) => {
+          if (index <= paras -1) {
+            return (
+              <article key={index}>
+                <h5>{lorem}</h5>
+              </article>
+            )
+          }
+        })}
+      </div>
+    </>
     )
 }
 
