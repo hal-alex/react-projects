acfeb8505325d63fab4167f6049daf9d3da795a2
Alex <1995aha@gmail.com>
2022-09-30 15:22:43 +0100

    finished project 11

--- Project 11/src/Navbar.js
+++ Project 11/src/Navbar.js
@@ -4,7 +4,49 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+
+  const [displayLinks, setDisplayLinks] = useState(false)
+
+  return (
+    <>
+      <nav>
+        <div className="nav-center">
+          <div className="nav-headers">
+            <img src={logo} alt="logo" />
+            <button className="nav-toggle" onClick={() => setDisplayLinks(!displayLinks)}>
+              <FaBars></FaBars>
+            </button>
+          </div>
+          <div className="links-container show-container"
+
+          >
+            <ul className="links">
+              {links.map((link) => {
+                const { id, url, text} = link
+                return (
+                    <li key={id}>
+                      <a href={url}> {text} </a>
+                    </li>
+                )
+              })}
+            </ul>
+          </div>
+          <ul className="social-icons">
+              {social.map((link) => {
+                const { id, url, icon } = link
+                return (
+                  <li key={id}>
+                    <a href={url}> {icon} </a>
+                  </li>
+                )
+              })}
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
 }
 
 export default Navbar
+
+ 
