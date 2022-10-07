e9173ed65c4b1873913e573ee647723e6018c4bc
Alex <1995aha@gmail.com>
2022-10-07 19:12:20 +0100

    working on modal container visibility

--- Project 12/src/Modal.js
+++ Project 12/src/Modal.js
@@ -1,7 +1,45 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 const Modal = () => {
-  return <h2>Modal</h2>
+
+
+  return (
+    <>
+      <div className={`modal-overlay show-modal`}>
+
+          <div className='modal-container'>
+            Testing <button><FaTimes></FaTimes></button>
+          </div>
+        
+    </div>
+
+
+    </>
+
+
+  )
 }
 
 export default Modal
+
+// const Modal = () => {
+
+//   const [showModal, setShowModal] = useState(false)
+
+//   return (
+//     <>
+//       <button className='btn' onClick={() => setShowModal(!showModal)}>show modal</button>
+//       {showModal && (
+//         <div className='modal-container'>
+//           Testing <button onClick={() => setShowModal(!showModal)}><FaTimes></FaTimes></button>
+//         </div>
+//       )}
+
+//     </>
+
+
+//   )
+// }
+
+// export default Modal
+
