5ed8c93d746d5e7bfe3223873febadb721e1e995
Alex <1995aha@gmail.com>
2022-10-08 13:00:31 +0100

    added content for sidebar

--- Project 12/src/Sidebar.js
+++ Project 12/src/Sidebar.js
@@ -4,7 +4,35 @@ import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
+  return (
+    <aside className={`sidebar show-sidebar`}>
+      <div className="sidebar-header">
+        <img src={logo} alt="logo" className="logo" />
+        <button className="close-btn"> <FaTimes></FaTimes> </button>
+      </div>
+      <ul className='links'>
+        {links.map((link) => {
+          const { id, url, text, icon } = link
+          return (
+            <li key={id}>
+              <a href={url}> {icon} {text}</a>
+            </li>
+          )
+        })}
+      </ul>
+      <ul className='social-icons'>
+        {social.map((item) => {
+          const { id, url, icon } = item
+          return (
+            <li key={id}>
+              <a href={url}> {icon} </a>
+            </li>
+          )
+        })}
+      </ul>
+
+    </aside>
+  )
 }
 
 export default Sidebar
